ff191a7fdfccf55af62c703a2b0f7c273c5c171b
Felipe300 <caracoloro@gmail.com>
2022-07-02 17:38:12 -0400

    feat: Create PrismaClient config

--- src/server/createContex.ts
+++ src/server/createContex.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../utils/prisma'
 
 export function createContext ({ req, res }:
   {req: NextApiRequest, res:NextApiResponse}) {
-  return { req, res }
+  return { req, res, prisma }
 }
 
 export type Context = ReturnType<typeof createContext>
