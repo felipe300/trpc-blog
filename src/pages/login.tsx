5009e2371eb9f148dbbce03e2ea7ffb9bdf6e59a
Felipe300 <caracoloro@gmail.com>
2022-07-04 21:26:12 -0400

    refactor: Create a FormComponent

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,37 +1,13 @@
-import Link from 'next/link'
-import { useForm } from 'react-hook-form'
-import { CreateUserInput } from '../schema/user.schema'
-// import { useRouter } from 'next/router'
-// import { trpc } from '../utils/trpc'
+import dynamic from 'next/dynamic'
+
+const LoginForm = dynamic(() => import('../components/LoginForm'), {
+  ssr: false
+})
 
 const LoginPage = () => {
-  const { handleSubmit, register } = useForm<CreateUserInput>()
-  // const router = useRouter()
-
-  // const { mutate, error } = trpc.useMutation(['users.register-user'], {
-  //   onSuccess: () => {
-  //     router.push('/login')
-  //   }
-  // })
-
-  function onSubmit (values: CreateUserInput) {
-    // mutate(values)
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      {/* {error && <p>{error.message}</p>} */}
-
-      <h1>Login</h1>
-      <input
-        type='email'
-        placeholder='john.smith@example.com'
-        {...register('email')} />
-      </form>
-      <Link href='/register'>Register</Link>
-    </>
-  )
+  return <div>
+    <LoginForm />
+  </div>
 }
 
 export default LoginPage
